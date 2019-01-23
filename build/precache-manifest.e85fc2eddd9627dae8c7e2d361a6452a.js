d830592dc52fab945649553b644f98502714a93d
Richard Widjaya <pp25253@gmail.com>
2019-01-23 18:13:58 +0800

    Updating App File

---
+++ build/precache-manifest.e85fc2eddd9627dae8c7e2d361a6452a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3baa45f1f8895a523f41",
+    "url": "/static/css/main.d1dedac2.chunk.css"
+  },
+  {
+    "revision": "3baa45f1f8895a523f41",
+    "url": "/static/js/main.3baa45f1.chunk.js"
+  },
+  {
+    "revision": "554acb3b23ed244b46cb",
+    "url": "/static/css/1.d46c1741.chunk.css"
+  },
+  {
+    "revision": "554acb3b23ed244b46cb",
+    "url": "/static/js/1.554acb3b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12622ada44bf26aecf5513f716116199",
+    "url": "/static/media/wds011402.12622ada.woff"
+  },
+  {
+    "revision": "56d4e0b8156d2e2209e7feff88cc9ae2",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/Scroll.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Scroll = (props) => {
+	return (
+		<div style={{ overflow: 'scroll', 
+						border: '5px solid gray',
+						height: '800px',
+						margin: '5px'  }}>
+			{props.children}
+		</div>
+		);
+};
+
+export default Scroll;
